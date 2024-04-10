1740763987dc7cdc7484ff05339df52914647ddf
Kornel <k.uzarowski@gmail.com>
2024-04-10 19:40:40 +0200

    initial commit

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -35,7 +35,7 @@ function SearchBar() {
             displayValue={(item: string) => item}
             onChange={(event) => setQuery(event.target.value)}
           />
-
+          {/*Commit*/}
           <Combobox.Options
             static
             className=" p-2 w-full absolute  mt-2 max-h-60  overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
